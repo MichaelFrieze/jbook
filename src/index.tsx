69c2e6dc61e9045ee07bcc8cbe79b6da4e97518d
Michael Frieze <michaelfrieze@icloud.com>
2021-11-10 17:07:08 -0500

    created a basic form element

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hello World</div>;
+  const [input, setInput] = useState('');
+  const [code] = useState('');
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
